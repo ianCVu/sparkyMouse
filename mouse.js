a8f61c2a3930397ffa2bd542196645b3f0fcd227
Ian Vu <ianv6207@mytusd.org>
2026-03-29 12:01:35 -0700

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -1,1 +1,19 @@
-null
+const mouse = document.createElement("img");
+mouse.style.position = "fixed";
+mouse.style.zIndex = 999999;
+mouse.width = "50";
+mouse.height = "50";
+mouse.src = "https://github.com/ianCVu/sparkyMouse/blob/main/mouse.gif?raw=true";
+window.addEventListener("mousemove", function(event) {
+  mouse.style.top = event.clientY + "px";
+  mouse.style.left = event.clientX + "px";
+});
+window.addEventListener("mousedown", function(event) {
+  mouse.width = "100";
+  mouse.height = "100";
+});
+window.addEventListener("mouseup", function(event) {
+  mouse.width = "50";
+  mouse.height = "50";
+});
+document.body.appendChild(mouse);
